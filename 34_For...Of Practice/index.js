b758061aeef8a83eb8fa3d47fb9a877c5a1c8e63
leekoby <abc2752@naver.com>
2022-12-21 22:25:23 +0900

    Create index.js

---
+++ 34_For...Of Practice/index.js
@@ -0,0 +1,18 @@
+/* Your output should look like this:
+
+1
+4
+9
+16
+25
+36
+49
+64
+81
+*/
+// WRITE YOUR LOOP BELOW THIS LINE:
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //DON'T CHANGE THIS LINE PLEASE!
+for (let i of numbers) {
+    console.log(i * i);
+}
+
