5416e8c4d268bb882c3b3ce653cd4e0fbbf23bda
leekoby <abc2752@naver.com>
2022-12-25 19:49:50 +0900

    Create Sum Array Exercise.js

---
+++ 42_Sum Array Exercise/Sum Array Exercise.js
@@ -0,0 +1,26 @@
+/** Sum Array Exercise
+Write a function called sumArray which accepts a single argument: an array of numbers.  It should return the sum of all the numbers in the array.
+
+sumArray([1,2,3]) // 6
+sumArray([2,2,2,2]) // 8
+sumArray([50,50,1]) // 101
+Hints:
+
+You'll need a variable to keep track of the total.  It should start out as zero.
+
+Loop over the array and for each element, add it to the total variable.
+
+After you have added every number to total, return total.*/
+
+function sumArray(arr) {
+
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+
+}
+
+
