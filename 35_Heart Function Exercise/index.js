c0599d12c6d8cf433cb69a922b1b31b2e82a7ae1
leekoby <abc2752@naver.com>
2022-12-25 16:21:16 +0900

    Create index.js

---
+++ 35_Heart Function Exercise/index.js
@@ -0,0 +1,12 @@
+/* It's time to write our first function!
+
+Define a function called printHeart that prints out the string "<3"
+
+Execute your function once*/
+
+// Write your function here:
+function printHeart() {
+    console.log("<3");
+}
+
+printHeart()
