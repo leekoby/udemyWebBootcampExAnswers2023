d66a459c453df4c78cbfbd8ba8bb7f447b28d1cb
leekoby <abc2752@naver.com>
2023-01-05 14:52:15 +0900

    Manipulating Attributes Practice

---
+++ 54_Manipulating Attributes Practice/app.js
@@ -0,0 +1,4 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+
+document.querySelector('img').src = 'https://devsprouthosting.com/images/chicken.jpg';
+document.querySelector('img').alt = 'chicken'
